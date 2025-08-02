7883ea05eb8e77dff01397f5c9ea4b0cff5864e4
Gautham <gautham.dep@gmail.com>
2025-08-02 14:26:10 +0800

    Initial Commit

---
+++ Stick man project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stick man project
+//
+//  Created by Gautham Dinakaran on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stick man project/Stick_man_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stick_man_projectApp.swift
+//  Stick man project
+//
+//  Created by Gautham Dinakaran on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Stick_man_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
